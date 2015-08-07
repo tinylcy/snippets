b87092cc03c021e41bafe96593efd5d071ed6d4b
tinylcy <tinylcy@yeah.net>
2015-08-07 21:09:23 +0800

    Update RotateImage_1.cpp
    最笨的方法

--- 048-Rotate-Image/RotateImage_1.cpp
+++ 048-Rotate-Image/RotateImage_1.cpp
@@ -43,3 +43,35 @@ public:
         }
     }
 };
+
+int main()
+{
+    vector<int> v1;
+    v1.push_back(1);
+    v1.push_back(2);
+    v1.push_back(3);
+    v1.push_back(4);
+    vector<int> v2;
+    v2.push_back(5);
+    v2.push_back(6);
+    v2.push_back(7);
+    v2.push_back(8);
+    vector<int> v3;
+    v3.push_back(9);
+    v3.push_back(10);
+    v3.push_back(11);
+    v3.push_back(12);
+    vector<int> v4;
+    v4.push_back(13);
+    v4.push_back(14);
+    v4.push_back(15);
+    v4.push_back(16);
+    vector<vector<int> > matrix;
+    matrix.push_back(v1);
+    matrix.push_back(v2);
+    matrix.push_back(v3);
+    matrix.push_back(v4);
+    Solution object;
+    object.rotate(matrix);
+    return 0;
+}

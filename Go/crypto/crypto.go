98f01ee774d5f838a49f890b6a33e5002db241cc
tinylcy <tinylcy@gmail.com>
2018-03-04 20:58:31 +0800

    Add crypto comments.

--- Go/crypto/crypto.go
+++ Go/crypto/crypto.go
@@ -33,35 +33,47 @@ func main() {
 	label := []byte("")
 	hash := sha256.New()
 
-	cipherText, err := rsa.EncryptOAEP(hash, rand.Reader, publicKeyTwo, message, label)
+	// EncryOAEP encrypts the given message with RSA-OAEP
+	// OAEP is parameterised by a hash function that is used as a random oracle.
+	// Encryption and decryption of a given message must use the same hash function
+	// and sha256.New() is a reasoneble choice.
+	//
+	// The random parameter is used as a source of entropy to ensure that encrypting
+	// the same message twice doesn't result in the same ciphertext.
+	//
+	// The label parameter may contain arbitrary data that will not be encrypted, but
+	// which gives important context to the message.
+	ciphertext, err := rsa.EncryptOAEP(hash, rand.Reader, publicKeyTwo, message, label)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("OAEP encrypted [%s] to \n[%x]\n", string(message), cipherText)
+	fmt.Printf("OAEP encrypted [%s] to \n[%x]\n", string(message), ciphertext)
 	fmt.Println()
 
 	// Message - Signature
 	var opts rsa.PSSOptions
 	opts.SaltLength = rsa.PSSSaltLengthAuto
 	PSSmessage := message
-	newhash := crypto.SHA256
+	newhash := crypto.SHA256 // Hash function
 	pssh := newhash.New()
 	pssh.Write(PSSmessage)
-	hashed := pssh.Sum(nil)
+	hashed := pssh.Sum(nil) // The result of hashing message
 
+	// SignPSS calculates the signature of hashed using RSASSA-PSS.
+	// Note that hashed must be the result of hashing the input message using the given hash function.
 	signature, err := rsa.SignPSS(rand.Reader, privateKeyOne, newhash, hashed, &opts)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("PSS signature: %x\n", signature)
 
-	plainText, err := rsa.DecryptOAEP(hash, rand.Reader, privateKeyTwo, cipherText, label)
+	plaintext, err := rsa.DecryptOAEP(hash, rand.Reader, privateKeyTwo, ciphertext, label)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("OAPE decrypted [%x] to \n [%s]\n", cipherText, plainText)
+	fmt.Printf("OAPE decrypted [%x] to \n [%s]\n", ciphertext, plaintext)
 
 	err = rsa.VerifyPSS(publicKeyOne, newhash, hashed, signature, &opts)
 	if err != nil {
